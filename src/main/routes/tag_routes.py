7a55141523ca437b316cea2088cc8a5316fecc1a
daniel-zacarias <daniel.zacarias@bantec.com.br>
2024-02-10 18:16:30 -0300

    feat: Implemeting Validator

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 
 from src.errors.error_handler import handle_errors
+from src.validators.tag_creator_validator import tag_creator_validator
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 
@@ -10,6 +11,7 @@ tag_routes_bp = Blueprint('tags_routes', __name__)
 @tag_routes_bp.route('/create_tag', methods=['POST'])
 def create_tags():
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
 
         http_request = HttpRequest(body=request.json)
